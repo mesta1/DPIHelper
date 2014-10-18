680a001a271856c4b98ee86a9eb2c5a0c0649244
Michele Cattafesta <michelecatta@gmail.com>
2014-10-18 10:20:26 +0200

    1st commit
    Signed-off-by: Michele Cattafesta <michelecatta@gmail.com>

---
+++ DPIHelper/DpiDecorator.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace DPIHelper
+{
+    public class DpiDecorator : Decorator
+    {
+        public DpiDecorator()
+        {
+            this.Loaded += (s, e) =>
+            {
+                Matrix m = PresentationSource.FromVisual(this).CompositionTarget.TransformToDevice;
+                ScaleTransform dpiTransform = new ScaleTransform(1 / m.M11, 1 / m.M22);
+                if (dpiTransform.CanFreeze)
+                    dpiTransform.Freeze();
+                this.LayoutTransform = dpiTransform;
+            };
+        }
+    }
+}
